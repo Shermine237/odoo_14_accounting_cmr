45d89202e3dfe18ff9f7e0a20f737f0542b4d3b6
Charlie Rostant YOSSA <charlieyossa@mavou.consulting>
2024-01-30 12:28:58 +0100

    [FIX]

--- base_accounting_kit/report/report_financial.py
+++ base_accounting_kit/report/report_financial.py
@@ -78,11 +78,38 @@ class AccountFinancialReport(models.Model):
     account_report_id = fields.Many2one(
         'account.financial.report',
         'Report Value')
-    account_type_ids = fields.Many2many(
-        'account.account.type',
-        'account_account_financial_report_type',
-        'report_id', 'account_type_id',
-        'Account Types')
+    # account_type_ids = fields.Many2many(
+    #     'account.account.type',
+    #     'account_account_financial_report_type',
+    #     'report_id', 'account_type_id',
+    #     'Account Types')
+
+    account_type_ids = fields.Selection(
+        selection=[
+            ("asset_receivable", "Receivable"),
+            ("asset_cash", "Bank and Cash"),
+            ("asset_current", "Current Assets"),
+            ("asset_non_current", "Non-current Assets"),
+            ("asset_prepayments", "Prepayments"),
+            ("asset_fixed", "Fixed Assets"),
+            ("liability_payable", "Payable"),
+            ("liability_credit_card", "Credit Card"),
+            ("liability_current", "Current Liabilities"),
+            ("liability_non_current", "Non-current Liabilities"),
+            ("equity", "Equity"),
+            ("equity_unaffected", "Current Year Earnings"),
+            ("income", "Income"),
+            ("income_other", "Other Income"),
+            ("expense", "Expenses"),
+            ("expense_depreciation", "Depreciation"),
+            ("expense_direct_cost", "Cost of Revenue"),
+            ("off_balance", "Off-Balance Sheet"),
+        ],
+        string="Type",
+        help="These types are defined according to your country. The type contains more information " \
+             "about the account and its specificities."
+    )
+    
     sign = fields.Selection(
         [("-1", 'Reverse balance sign'), ("1", 'Preserve balance sign')],
         'Sign on Reports', required=True, default="1",
