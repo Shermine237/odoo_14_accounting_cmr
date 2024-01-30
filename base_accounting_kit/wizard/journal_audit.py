76be7a1347ea045315a9db59baffbeea658693a4
Charlie Rostant YOSSA <charlieyossa@mavou.consulting>
2024-01-30 11:13:46 +0100

    [FIX]

--- base_accounting_kit/wizard/journal_audit.py
+++ base_accounting_kit/wizard/journal_audit.py
@@ -16,11 +16,19 @@ class AccountPrintJournal(models.TransientModel):
     sort_selection = fields.Selection(
         [('date', 'Date'), ('move_name', 'Journal Entry Number')],
         'Entries Sorted by', required=True, default='move_name')
-    journal_ids = fields.Many2many(comodel_name='account.journal', relation='account_print_journel_rel', string='Journals',
+    journal_ids = fields.Many2many('account.journal', string='Journals',
                                    required=True,
                                    default=lambda self: self.env[
                                        'account.journal'].search(
                                        [('type', 'in', ['sale', 'purchase'])]))
+    
+    account_control_ids = fields.Many2many(
+        comodel_name='account.account',
+        relation='print_journal_account_control_rel',
+        column1='print_journal_id',
+        column2='account_control_id',
+        string='Account Controls'
+    )
 
     def _print_report(self, data):
         data = self.pre_print_report(data)
